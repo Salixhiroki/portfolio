84d8bd1716cf327e9ae8c3fd55b74ab286364c61
Tobita_Hiroki <salix.hiro@gmail.com>
2020-01-21 14:16:52 +0000

    portfolio

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,20 @@
 class RecipesController < ApplicationController
   def new
+    # @recipe=Recipe.new
   end
+  
+  def create
+    
+  end
+  
+  
+  def index
+    # @recipes=Recipes.all.order(created_at: :desc)
+  end
+  
+  def show
+    # @detail=Recipe.find_by(id: params[:id])
+    # @user=User.find_by(id: @detail.user_id)
+  end
+  
 end
