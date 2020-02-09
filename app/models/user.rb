2d994037b6dbc3aed26115913948678ab9d40035
Tobita_Hiroki <salix.hiro@gmail.com>
2020-02-09 05:07:39 +0000

    portfolio

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+
+  validates :name, presence: true, length: {maximum: 15}
+  validates :email, presence: true
+  validates :password, presence: true, length: {in: 8..32}
+  validates :password_digest, presence: true
+  
+  
+  
+  
+  has_secure_password
+
 end
