1695dfeaf28b38fe19437ab17e9db2fefc83a0ea
Tobita_Hiroki <salix.hiro@gmail.com>
2020-05-09 06:58:01 +0000

    heroku push twice

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,9 @@ module Mychazuke
     config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.web_console.whitelisted_ips =  '58.89.116.53'
+    unless Rails.env.production?
+      config.web_console.whitelisted_ips =  '58.89.116.53'
+    end
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
     # Settings in config/environments/* take precedence over those specified here.
