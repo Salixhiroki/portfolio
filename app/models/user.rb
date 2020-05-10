5f922ca68a58754c16f7b4670827d7f0d50e038f
Tobita_Hiroki <salix.hiro@gmail.com>
2020-05-10 12:22:40 +0000

    heroku push nineth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class User < ApplicationRecord
   with_options presence: true do
     validates :name
--- db/migrate/20200510092359_rename_password_digest_column.rb
+++
@@ -1,5 +0,0 @@
-class RenamePasswordDigestColumn < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :users, :password_digest, :password_digest
-  end
-end
