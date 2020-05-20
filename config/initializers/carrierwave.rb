14f4226e3344b12af9cf8c253264ed1f0fb16624
Salixhiroki <58902110+Salixhiroki@users.noreply.github.com>
2020-05-20 20:56:17 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,8 @@ unless Rails.env.development? || Rails.env.test?
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: "AWS",
-      aws_access_key_id: ENV["AKIA4IY7YPUCAINIS3X3"],
-      aws_secret_access_key: ENV["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"],
+      aws_access_key_id: ENV[""],
+      aws_secret_access_key: ENV[""],
       region: ENV["ap-northeast-1"]
     }
     
