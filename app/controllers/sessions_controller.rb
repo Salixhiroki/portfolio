75438119ba2720ca60ba4477cc3144717aebf86e
Tobita_Hiroki <salix.hiro@gmail.com>
2020-06-02 15:10:55 +0000

    modify_array

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,6 @@ class SessionsController < ApplicationController
   # アカウント新規作成
   def create
     user = User.find_by(email: session_params[:email])
-
     if user&.authenticate(session_params[:password])
       log_in user
       redirect_to recipes_path, success: 'ログインに成功しました'
